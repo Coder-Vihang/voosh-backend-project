1366d380c9d3a59faef902fbfd3a288730491ece
Vihang Subhash Hiwase <subhash.hiwase@medibuddy.in>
2024-04-07 15:27:09 +0530

    Addded COde to seriealize and de serialize the user

--- GoogleStratergy.js
+++ GoogleStratergy.js
@@ -9,3 +9,11 @@ passport.use(new GoogleStrategy({
   function(request, accessToken, refreshToken, profile, done) {
     return done(null, profile);
   }));
+
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
